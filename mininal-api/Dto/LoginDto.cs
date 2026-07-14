ca0cb4f87790722786505fc0ac8f38b978425032
valquiria <valquirianatalia0@gmail.com>
2026-07-13 23:08:41 -0300

    Adicionando rota de login e class LoginDto

---
+++ mininal-api/Dto/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace mininal_api.Dto;
+
+public class LoginDto
+{
+    public string Email { get; set; } = default;
+
+    public string Senha { get; set; } = default;
+}
--- mininal-api/Program.cs
+++ mininal-api/Program.cs
@@ -1,10 +1,20 @@
+using mininal_api.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/teste", () =>
-{
-    return "teste";
-});
 
+
+
+app.MapPost("/login", (LoginDto loginDto) =>
+{
+    if (loginDto.Email == "Valquirianatalia@gmail.com" && loginDto.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso ");
+    }
+
+    return Results.Unauthorized();
+
+});
 
 app.Run();
